f111b16d742482b73763459d74eb96e68b3e8253
Carlos Vallejo <carlos.vallejo@101grados.com>
2021-07-04 19:41:25 -0500

    ElTelegrafo actualidad

---
+++ news/news/spiders/el_telegrafo_actualidad.py
@@ -0,0 +1,58 @@
+import scrapy
+import w3lib.html
+
+class ElComercioSpider(scrapy.Spider):
+  name = "el_telegrafo_actualidad"
+  start_urls = [
+    'https://www.eltelegrafo.com.ec/contenido/categoria/44/actualidad'
+  ]
+  custom_settings = {
+    'FEED_URI': 'el-telegrafo-actualidad.json',
+    'FEED_FORMAT': 'json',
+    'MEMUSAGE_NOTIFY_MAIL': [
+      'cvallejo.ec@gmail.com'
+    ],
+    'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ',
+    'FEED_EXPORT_ENCODING': 'utf-8',
+  }
+
+  LINKS = '//div[@id="k2Container"]//div[@class="row"]//article/h1/a/@href'
+  TITLE = '//article[contains(@class, "full-article")]//h1[@class="story-heading"]/span/text()'
+  PARAGRAPHS = '//div[@class="itemFullText"]//p'
+  # PARAGRAPHS = '//*[not(self::div[contains(@class, "twitter-tweet")])]//div[@class="itemFullText"]//p'
+  # PARAGRAPHS = '//*[not(self::iframe[@id="twitter-widget-2"])]//div[@class="itemFullText"]//p'
+  TIME = '//article[contains(@class, "full-article")]//span[contains(@class, "story-publishup")]//text()'
+  
+  def parse(self, response):
+    news_links = response.xpath(self.LINKS).getall()
+
+    for link in news_links:
+      yield response.follow(link, callback=self.parse_full_new, cb_kwargs={ 'url': link })
+
+  def parse_full_new(self, response, **kwargs):
+    link = kwargs["url"]
+    title = response.xpath(self.TITLE).get()
+
+    paragraphs_raw = response.xpath(self.PARAGRAPHS).getall()
+    time_raw = response.xpath(self.TIME).getall()
+    time = time_raw[0].strip() + ' ' + time_raw[1].strip()
+
+    category = "Actualidad"
+
+    body = []
+
+    for paragraph in paragraphs_raw:
+      output = w3lib.html.remove_tags(paragraph)
+      body.append(output)
+      
+    yield {
+      'url': 'https://www.eltelegrafo.com.ec/' + link,
+      'title': title,
+      'time': time,
+      'category': category,
+      'body': body,
+    }
+
+
+    
+
